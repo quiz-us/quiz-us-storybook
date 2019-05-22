7d839c1208f2e1c5e3f6919c28b5ba3f1a2839f2
Joshua Ling <joshling1919@gmail.com>
2019-05-21 18:10:14 -0700

    Add material-ui styles

--- quiz-us-components/QuestionMultipleChoice/QuestionMultipleChoice.js
+++ quiz-us-components/QuestionMultipleChoice/QuestionMultipleChoice.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { styled } from '@material-ui/styles';
 import PropTypes from 'prop-types';
+
+const Answer = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  border: '1px solid gray',
+  borderRadius: 3,
+  minHeight: 48,
+  padding: '0 30px',
+  marginTop: '10px',
+  cursor: 'pointer'
+});
 
 const QuestionMultipleChoice = ({ question, answers }) => {
   return (
     <div>
       {question}
       {answers.map(({ text }) => {
-        return <div key={text}>{text}</div>;
+        return <Answer key={text}>{text}</Answer>;
       })}
     </div>
   );
