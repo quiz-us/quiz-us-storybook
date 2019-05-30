2db73572000d1ba3111f03259d1a47c6ff54fbba
Joshua Ling <joshling1919@gmail.com>
2019-05-30 09:05:38 -0700

    Fix external rollup dependencies

--- quiz-us-components/rollup.config.js
+++ quiz-us-components/rollup.config.js
@@ -2,11 +2,19 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 
-import pkg from './package.json';
+import { createFilter } from 'rollup-pluginutils';
+
+const external = createFilter(
+  ['prop-types', 'react', 'react-dom', '@material-ui/**'],
+  null,
+  { resolve: false }
+);
+// {resolve: false} will make sure these filters are not passed to
+// path.resolve first and resolved against the current working directory
 
 module.exports = {
   input: 'src/index.js',
-  external: Object.keys(pkg.peerDependencies),
+  external,
   output: {
     file: 'dist/index.js',
     format: 'cjs'
