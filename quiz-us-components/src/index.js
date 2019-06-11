6997ed391ec656e25e1fd17a008bc0c89247116b
Joshua Ling <joshling1919@gmail.com>
2019-06-11 08:48:13 -0700

    Export RichTextEditor

--- quiz-us-components/src/index.js
+++ quiz-us-components/src/index.js
@@ -1,5 +1,8 @@
+import RichTextEditor from '../../stories/RichTextEditor/index';
 export {
   default as QuestionMultipleChoice
 } from './components/QuestionMultipleChoice';
+
+export { default as RichTextEditor } from './components/RichTextEditor';
 
 export { default as QuizUsTheme } from './theme';
--- stories/RichTextEditor/index.js
+++ stories/RichTextEditor/index.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import withReadme from "storybook-readme/with-readme";
-import Readme from "../../quiz-us-components/src/components/RichTextEditor/README.md";
-import RichTextEditor from "../../quiz-us-components/src/components/RichTextEditor";
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import withReadme from 'storybook-readme/with-readme';
+import Readme from '../../quiz-us-components/src/components/RichTextEditor/README.md';
+import { RichTextEditor } from '../../quiz-us-components/src';
 
 export default () => {
-  storiesOf("Rich Text RichTextEditor", module)
+  storiesOf('Rich Text RichTextEditor', module)
     .addDecorator(withReadme(Readme))
-    .add("RichTextEditor", () => <RichTextEditor />);
+    .add('RichTextEditor', () => <RichTextEditor />);
 };
