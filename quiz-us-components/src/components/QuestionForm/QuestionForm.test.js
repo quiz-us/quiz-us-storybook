3e35f103045a1439480ce7d393bbf4f84a52e95d
Joshua Ling <joshling1919@gmail.com>
2019-07-03 12:29:47 -0700

    Start QuestionForm test

---
+++ quiz-us-components/src/components/QuestionForm/QuestionForm.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import QuestionForm from './QuestionForm';
+
+describe('<QuestionForm />', () => {
+  it('starts with the correct initial inputs state', () => {
+    // const component = (
+    //   <QuestionForm
+    //     standards={[{ id: 1, name: '1' }, { id: 2, name: '2' }]}
+    //     questionTypes={['Multiple Choice', 'Free Response']}
+    //   />
+    // );
+    // const wrapper = mount(component);
+    // console.log(wrapper);
+    // expect(wrapper).toBe(true);
+  });
+});
