62f01fd242fb674856c5d0117a62cf9016677a69
Joshua Ling <joshling1919@gmail.com>
2019-07-09 07:31:51 -0700

    Make script name more clear

--- scripts/setUpCircle.sh
+++ scripts/setUpCircle.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# This script is used to set up environment for CircleCI
 echo -e "Installing node_modules for storybook dir..."
 npm ci
 echo -e "Installing node_modules inner component library dir..."
