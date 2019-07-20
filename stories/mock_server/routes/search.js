4e3bd2c5b601c656dbfab55bbb25e49f6fdfa8b0
Joshua Ling <joshling1919@gmail.com>
2019-07-19 19:53:48 -0700

    Finish mock search core logic

--- stories/mock_server/routes/search.js
+++ stories/mock_server/routes/search.js
@@ -1,18 +1,38 @@
+import stringSimilarity from 'string-similarity';
 import questions from '../data/questions';
 
 const search = filters => {
-  const { standard, question, tags } = filters;
-  let results = [];
+  const { standard, question, tag } = JSON.parse(filters);
+  let results = questions;
 
   if (standard) {
-    results = questions.filter(q => q.standard === standard);
+    results = results.filter(q => q.standard === standard);
   }
-  /** @todo: more filtering here */
+
+  if (tag) {
+    results = results.filter(({ tags }) => {
+      for (let i = 0; i < tags.length; i += 1) {
+        const t = tags[i];
+        if (stringSimilarity.compareTwoStrings(t, tag) > 0.5) {
+          return true;
+        }
+      }
+      return false;
+    });
+  }
+  if (question) {
+    results = results.filter(
+      q => stringSimilarity.compareTwoStrings(q.question, question) > 0.5
+    );
+  }
+
+  return results;
 };
 export default mock => {
   mock.post('/search', (req, res) => {
     const { _body } = req;
     const searchResults = search(_body);
-    return res.status(200).body('yay');
+    console.log('HERE WERE MATCHES', searchResults);
+    return res.status(200).body(JSON.stringify(searchResults));
   });
 };
