21a2c976e0257e4bb83da730d128daaa35f4dd06
Joshua Ling <joshling1919@gmail.com>
2019-07-24 03:29:37 -0700

    Fix hot reloading

--- stories/mock_server/index.js
+++ stories/mock_server/index.js
@@ -1,9 +1,13 @@
-import mock from 'xhr-mock';
+import mock, { proxy } from 'xhr-mock';
 import TagsRoutes from './routes/tags';
 import SearchRoutes from './routes/search';
 
 export default () => {
   mock.setup();
+  // Set up routes that need to be mocked:
   TagsRoutes(mock);
   SearchRoutes(mock);
+
+  // proxy unhandled requests to the real servers:
+  mock.use(proxy);
 };
