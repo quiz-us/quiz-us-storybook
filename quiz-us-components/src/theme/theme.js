c47680b5bed47184bb3cc1cdd27e808639a7aa59
Joshua Ling <joshling1919@gmail.com>
2019-08-21 22:01:21 -0700

    Publish 0.0.15

--- quiz-us-components/src/theme/theme.js
+++ quiz-us-components/src/theme/theme.js
@@ -1,10 +1,10 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 import teal from '@material-ui/core/colors/teal';
-import purple from '@material-ui/core/colors/purple';
+import amber from '@material-ui/core/colors/amber';
 
 export default createMuiTheme({
   palette: {
     primary: teal,
-    secondary: purple
+    secondary: amber
   }
 });
